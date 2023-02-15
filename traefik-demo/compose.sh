799a6104fcf2e9e6f780e1e0ad6b5d7a3dc797ad
theJimboJones <james.m.maciejewski@gmail.com>
2023-02-15 05:53:02 +0000

    merge

--- traefik-demo/compose.sh
+++ traefik-demo/compose.sh
@@ -3,4 +3,4 @@ docker stop $($docker ps -aq --filter "label=$(basename "$PWD")")
 docker rm $($docker ps -aq --filter "label=$(basename "$PWD")")
 
 # start service, in detached mode, building images before starting and 
-docker-compose up -build -d
+docker-compose up --build -d
