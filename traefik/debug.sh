71ef89d5b91436d6e18de2bdba729a5ced323914
tehJimboJones <james.m.maciejewski@gmail.com>
2023-02-15 22:47:03 -0600

    fix treafik scripts and config

--- traefik/debug.sh
+++ traefik/debug.sh
@@ -1,3 +1,3 @@
 # Enter the of the image for the current folder
 
-docker exec -it $(docker ps --filter "name=$(basename "$PWD"")" -lw) /bin/bash
+docker exec -it $(docker ps --filter "name=$(basename "$PWD"")" -lq) /bin/bash
