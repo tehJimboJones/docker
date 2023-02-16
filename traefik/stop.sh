90a06dc1b54adfaaad77dcc1ecb8b99e170889c5
tehJimboJones <james.m.maciejewski@gmail.com>
2023-02-16 00:23:47 -0600

    fixes

--- traefik/stop.sh
+++ traefik/stop.sh
@@ -1,12 +1,18 @@
-
 # Stop all containers for the current service and remove containers
 
 list=$(docker ps -aq --filter label=$(basename $(pwd)))
-echo "Containers to stop: " $list
+if [-z $list]
+then
+    echo "No containers to stop and remove!"
 
-echo "Stopping containers..."
-docker stop $list
+else
+    echo "Containers to stop: " $list
 
-echo "Removing containers..."
-#docker stop $($docker ps -aq --filter "label=$(basename "$PWD")")
-docker rm $list
+    echo "Stopping containers..."
+    docker stop $list
+
+    echo "Removing containers..."
+    #docker stop $($docker ps -aq --filter "label=$(basename "$PWD")")
+    docker rm $list
+
+fi
